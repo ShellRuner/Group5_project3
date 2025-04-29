0587da3c08edaefe9ad99ea8819be9a2c57980aa
Muhammad Fawwaz Bashir <muhdfawwazbashir@gmail.com>
2025-04-29 16:36:51 +0100

    ALT/SOE/024/2830 - Event Creation and listing endpoints

--- project3.py
+++ project3.py
@@ -1,3 +1,67 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Form
+
+from pydantic import BaseModel
+
+from typing import Annotated, Optional
+
+
 
 app = FastAPI()
+
+class Event(BaseModel):
+    title: str
+    description: str
+    date: Optional[str] = None
+    location: str
+    flyer: Optional[str] = None
+
+    
+
+class Response(BaseModel):
+    message: Optional[str] = None
+    has_error: bool = False
+    error_message: Optional[str] = None
+    data: Optional[Event] = None
+
+
+events: dict[str, Event] = {}
+
+
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Event Creation API"}
+
+@app.get("/events")
+def get_events():
+    return list(events.values())
+
+@app.get("/events/{id}")
+def get_event_by_id(id: str):
+    events = events.get(id)
+
+@app.post("/create_event")
+async def create_event(
+    title: Annotated[str, Form()],
+    description: Annotated[str, Form()],
+    date: Annotated[str, Form()],
+    location: Annotated[str, Form()],
+    flyer: Annotated[Optional[UploadFile], File()] = None
+):
+    if title in events:
+        return Response(
+            has_error=True,
+            error_message="Event with this title already exists."
+        )
+    
+    flyer_filename = flyer.filename if flyer else None
+    
+    event = Event(
+        title=title,
+        description=description,
+        date=date,
+        location=location,
+        flyer=flyer_filename
+    )
+    events[title] = event
+    return Response(message="Event created successfully", data=event)
